32bcd1aa0fd7020c86c8cf47528e27d65f6ed9c3
Nivaslak <134709899+Nivaslak@users.noreply.github.com>
2023-05-29 14:30:50 +0100

    Delete Task-1 directory

--- Task-1/hello_world.py
+++
@@ -1,6 +0,0 @@
-# To print users name and age
-name = input("Enter your name")
-print("Name:{}".format(name))
-age = int(input("Enter your age"))
-print("Age:{}".format(age))
-print("Hello World!")
--- Task-1/menu.py
+++
@@ -1,8 +0,0 @@
-# user to order their 3 favourite food items
-item1=input("Enter your 1st favourite food item")
-item2=input("Enter your 2nd favourite food item")
-item3=input("Enter your 3rd favourite food item")
-print("Order confirmation!You have ordered:") 
-print(item1)
-print(item2)
-print(item3)
